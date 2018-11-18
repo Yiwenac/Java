6662b2a09c82257f06a72508fa4d28263e6b2341
Yiwenac <38489313+Yiwenac@users.noreply.github.com>
2018-11-18 14:28:28 +0000

    Add files via upload

---
+++ SimpleExpressions.java
@@ -0,0 +1,11 @@
+class SimpleExpressions{
+
+  public static void main(String[] args){
+    System.out.println(("19-character string").length()); //length of string.
+    System.out.println(("abcd").charAt(3));// get a character.
+    System.out.println(("abcd").equals("abcd"));// whether they are equal.
+    System.out.println(("Hello, world!").substring(7,12));// substring from position a to the position before b.
+    System.out.println(("Hello, world!").substring(7));// substrng from position a to the end.
+
+  }
+}
