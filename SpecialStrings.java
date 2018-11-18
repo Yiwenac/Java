6da669d00045daaff10712b60191c3f8eda56d69
Yiwenac <38489313+Yiwenac@users.noreply.github.com>
2018-11-18 14:28:09 +0000

    Add files via upload

---
+++ SpecialStrings.java
@@ -0,0 +1,10 @@
+class SpecialStrings{
+
+  public static void main(String[] args){
+    System.out.println("Simple string.");
+    System.out.println("Say \"Hello.\""); // \" is a double quote.
+    System.out.println("Name: \tJohn"); // \t is tab.
+    System.out.println("\\Food\\Bar"); // \\ is \.
+    System.out.println("One thing\nThe other thing"); // \n is the next line.
+  }
+}
