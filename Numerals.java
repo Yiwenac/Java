e4d2e46e02c7e4c87acde8cfde01a01de6f66b4d
Yiwenac <38489313+Yiwenac@users.noreply.github.com>
2018-11-18 14:27:56 +0000

    Add files via upload

---
+++ Numerals.java
@@ -0,0 +1,10 @@
+class Numerals{
+
+  public static void main(String[] args){
+    System.out.println(42);
+    System.out.println(052);
+    System.out.println(0x2a);
+    System.out.println(3.145e1);
+    System.out.println(12345678901L);
+  }
+}
