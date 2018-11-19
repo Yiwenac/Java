42f760dc0db00dd1cc7119cc9451e6edcc088d0e
Yiwenac <38489313+Yiwenac@users.noreply.github.com>
2018-11-19 11:51:50 +0000

    Add files via upload

---
+++ Remainder.java
@@ -0,0 +1,7 @@
+class Remainder{
+
+  public static void main(String[] operands){
+    System.out.println(Integer.parseInt(operands[0]) % Integer.parseInt(operands[1]));
+    //Integer.parseInt() convert a string of digits to an Integer.
+  }
+}
